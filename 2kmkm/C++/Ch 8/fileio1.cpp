638921784075eec15fed15d54fba2314cc9d2351
2kmkm <02dlrudals@naver.com>
2022-11-13 20:56:31 +0900

    1113

---
+++ 2kmkm/C++/Ch 8/fileio1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+  ifstream inStream;  // 입력 파일 객체 선언 
+  ofstream outStream; // 출력 파일 객체 선언
+
+  inStream.open ("infile.txt");       // 입력 파일 열기
+  outStream.open("outfile.txt");  // 출력 파일 열기
+
+  string word;
+  
+  while ( !inStream.eof() )          // 입력 파일의 끝에 도달할 때까지 단어(word)를 반복해서 읽고
+   cout  << word << " ";   // 이 단어를 출력 파일에 공백으로 구분해서 출력
+
+  inStream.close();             // 입력 파일 닫기
+  outStream.close();  // 출력 파일 닫기
+
+  return 0;
+}
