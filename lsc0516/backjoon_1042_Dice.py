2033364fb435d11a3fff47c05ebb29e56d7e4ce3
lsc0516 <54504369+Elorrol@users.noreply.github.com>
2022-11-14 16:27:58 +0900

    2022.11.14

--- lsc0516/backjoon_1042_Dice.py
+++ lsc0516/backjoon_1042_Dice.py
@@ -10,3 +10,5 @@ if n>1:
     print(4*(arr[0]+arr[1]+arr[2])+(8*n-12)*(arr[0]+arr[1])+(5*n*n-16*n+12)*arr[0])
 else:
     print(sum(num)-max(num))
+    
+# 주사위의 합을 3가지 종류로 나눠서 각 임의의 크기에 생기는 종류를 곱해서 계산함
