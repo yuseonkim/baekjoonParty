b91e8e142db7ffb7322b02585fbba216119d2351
munjihye <87179769+mun-jihye@users.noreply.github.com>
2022-11-27 22:38:12 +0900

    2022.11.27

---
+++ munjihye/boj_10815.py
@@ -0,0 +1,20 @@
+import sys
+
+N = int(sys.stdin.readline())
+cards = sorted(list(map(int, sys.stdin.readline().split())))
+M = int(sys.stdin.readline())
+checks = list(map(int, sys.stdin.readline().split()))
+
+for check in checks:
+    low, high = 0, N-1  # cards의 이진 탐색 index
+    exist = False
+    while low <= high:
+        mid = (low + high) // 2
+        if cards[mid] > check:  # 중간 값보다 작다면
+            high = mid - 1  # 중간보다 왼쪽 한 칸 옆까지 탐색
+        elif cards[mid] < check:  # 중간 값보다 크다면
+            low = mid + 1  # 중간보다 오른쪽 한 칸 옆부터 탐색
+        else:
+            exist = True
+            break
+    print(1 if exist else 0, end=' ')
