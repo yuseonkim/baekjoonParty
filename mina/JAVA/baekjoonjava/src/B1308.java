40cec2e5a8c0f60a8a004e302c0a946d6b33d9f0
chaemina <103165845+chaemina@users.noreply.github.com>
2022-11-28 14:21:34 +0900

    Update B1308.java

--- mina/JAVA/baekjoonjava/src/B1308.java
+++ mina/JAVA/baekjoonjava/src/B1308.java
@@ -11,23 +11,35 @@ public class B1308 {
             day[1] =29; // 윤년의 경우 2월이 29일이다
         return day; //day를 리턴한다
     }
-    // 날짜수 합하는 메소드
+    
+    // 날짜수 합하는 메소드 (총일수를 더할 것임)
     static int sumday(int year, int month, int day){ //날짜를 받고
-        int days =0;
-        int[] date; //날짜를 배열에 저장한다
-        for(int i=1; i< year; i++){
-            date = ly(i); // ?
+        int days =0; //계속 누적되기 위해 반복문 밖에서 초기화 
+        int[] date; //해당 연도의 일자를 12달로 나누어 저장 
+        
+        for(int i=1; i< year; i++){ //해당 year은 전체가 아니니까 포함하지 않음
+            date = ly(i); // 윤년인지 구하는 메소드에 각 월별로 일수가 저장되어있음
+            //1년부터 ~ 해당연도까지 반복하면서
+            //예 ) date = ly(2000) , date = {31,28,31,30,31,30,31,31,30,31,30,31}
+            
             for(int j =0; j<12; j++){
-                days += date[j]; //?
+                days += date[j];  //각각 연도의 전체 일수(월별로)를 더해준다
+            //예)  {31,28,31,30,31,30,31,31,30,31,30,31} 를 해당 월까지 반복하며 day에 더해준다
+      
             }
-        } // ?
-        date = ly(year);
-        for(int i =0; i< month -1; i++){
-            day+= date[i];
         }
-        days += day;
+        
+        // 해당 year 반복문
+        date = ly(year); //윤년메소드로 위와 같이 배열 구함
+        for(int i =0; i< month -1; i++){ //예) 5월 27일이면 4월까지의 일수만 구한다
+            days += date[i];
+        } 
+        
+        days += day; //해당 일수 더하기 
         return days;
     }
+    
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // today
